aef367eda0da54d041d33dfa52662f8d04571fff
frostburner <villarafaeljoseph@gmail.com>
2024-01-20 11:03:13 +0800

    conditional rendering sang navbars sa artist kag organizer

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,84 @@
-import React,{useState, useContext} from 'react'
-import { AuthContext } from '../../Helpers/AuthContext';
-import { useNavigate, Link } from 'react-router-dom';
-import './Navbar.css'
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../Helpers/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const { authState, setAuthState } = useContext(AuthContext);
-    const id = authState.id;
-  
-    const handleLogout = () => {
-      localStorage.removeItem("accessToken");
-      setAuthState({
-        username: "",
-        id: 0,
-        role: "",
-        status: false,
-      });
-      localStorage.clear();
-      navigate("/");
-    };
+  const navigate = useNavigate();
+  const { authState, setAuthState } = useContext(AuthContext);
+  const id = authState.id;
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({
+      username: "",
+      id: 0,
+      role: "",
+      status: false,
+    });
+    localStorage.clear();
+    navigate("/");
+  };
+  console.log(authState.role);
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid px-5">
-        <Link path to="/home" className="navbar-brand text-white fw-bold">AudioHive</Link>
+        <Link path to="/home" className="navbar-brand text-white fw-bold">
+          AudioHive
+        </Link>
         <div>
           <div className="d-flex flex-row gap-4">
             <ul className="navbar-nav ms-auto mb-2 text-white gap-3">
-              <li className="nav-item" onClick={(()=> navigate('/home'))}>Home</li>
-              <li className="nav-item" onClick={(()=> navigate('/eventslist'))}>Events</li>
-              <li className="nav-item" onClick={(()=> navigate('/store'))}>Store</li>
+              <li className="nav-item" onClick={() => navigate("/home")}>
+                Home
+              </li>
+              <li className="nav-item" onClick={() => navigate("/eventslist")}>
+                Events
+              </li>
+              <li className="nav-item" onClick={() => navigate("/store")}>
+                Store
+              </li>
               {/* <li className="nav-item" onClick={(()=> navigate('/productslist'))}>Product List</li> */}
-              <li className="nav-item" onClick={(()=> navigate('/productbyuser'))}>My Products</li>
-              <li className="nav-item" onClick={(()=> navigate('/eventbyuser'))}>My Events</li>
-              <li className="nav-item" onClick={(()=> navigate(`/storecheckoutlist/${id}`))}>Purchase History</li>
+              {/* <li className="nav-item" onClick={(()=> navigate('/productbyuser'))}>My Products</li>
+              <li className="nav-item" onClick={(()=> navigate('/eventbyuser'))}>My Events</li> */}
+              {authState.role !== 2 && (
+                <li
+                  className="nav-item"
+                  onClick={() => navigate("/productbyuser")}
+                >
+                  My Products
+                </li>
+              )}
+              {authState.role !== 1 && (
+                <li
+                  className="nav-item"
+                  onClick={() => navigate("/eventbyuser")}
+                >
+                  My Events
+                </li>
+              )}
+              <li
+                className="nav-item"
+                onClick={() => navigate(`/storecheckoutlist/${id}`)}
+              >
+                Purchase History
+              </li>
             </ul>
             <ul className="navbar-nav mb-2 text-white gap-2">
-            {authState.username && (
+              {authState.username && (
                 <li>
                   <span className="fw-bold">Hi! {authState.username}</span>
                 </li>
               )}
-              <li className="nav-item" onClick={(()=> handleLogout())}>Logout</li>
+              <li className="nav-item" onClick={() => handleLogout()}>
+                Logout
+              </li>
             </ul>
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
