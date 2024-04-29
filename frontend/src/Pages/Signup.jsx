8719297c9ccd7b4185b3d0d4cfeb7c4d8ab5b96d
AeonFlux022 <tolentinokevinjan22@gmail.com>
2024-04-29 17:38:10 +0800

    signup ui

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-
+import "../index.css";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -64,62 +64,55 @@ const Signup = () => {
     { value: 2, label: "Organizer" },
   ];
 
-
   return (
-    <div className="signup-container">
-      <div className="box-container">
-        <form onSubmit={handleSubmit} autoComplete="off">
-          <div className="form-group">
-            <label className="form-label">First Name</label>
-            <input
-              type="text"
-              name="firstName"
-              className="form-control"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
+    <div className="d-flex signup-container">
+      <div className="col-6 left px-3">
+        <form onSubmit={handleSubmit} autoComplete="off" className="p-5">
+          <div className="d-flex gap-2 flex-direction-row mb-1">
+            <div className="form-group col-6">
+              <label className="form-label">First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                className="form-control"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group col-6">
+              <label className="form-label">Last Name</label>
+              <input
+                type="text"
+                name="lastName"
+                className="form-control"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label className="form-label">Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              className="form-control"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
+          <div className="d-flex gap-2 flex-direction-row mb-1">
+            <div className="form-group col-5">
+              <label className="form-label">Birthday</label>
+              <input
+                type="date"
+                name="birthday"
+                className="form-control"
+                value={formData.birthday}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group col-7">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label className="form-label">Username</label>
-            <input
-              type="text"
-              name="username"
-              className="form-control"
-              value={formData.username}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label className="form-label">Birthday</label>
-            <input
-              type="date"
-              name="birthday"
-              className="form-control"
-              value={formData.birthday}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-group">
+          <div className="form-group mb-1">
             <label className="form-label">Address</label>
             <input
               type="text"
@@ -129,34 +122,47 @@ const Signup = () => {
               onChange={handleChange}
             />
           </div>
-          <div className="form-group">
-            <label className="form-label">Email</label>
+          <div className="form-group mb-1">
+            <label className="form-label">Username</label>
             <input
-              type="email"
-              name="email"
+              type="text"
+              name="username"
               className="form-control"
-              value={formData.email}
+              value={formData.username}
               onChange={handleChange}
             />
           </div>
-          <div className="form-group">
-            <label className="form-label">What's your role?</label>
-            <select
-              name="role"
-              className="select-control"
-              value={formData.role}
+          <div className="form-group mb-1">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={formData.password}
               onChange={handleChange}
-            >
-              <option value="" hidden>
-                Account Type
-              </option>
-              {accountOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
+            />
           </div>
+          <div className="form-group mb-3">
+            <div className="column">
+              <label className="form-label">What's your role?</label>
+              <select
+                name="role"
+                className="form-select"
+                value={formData.role}
+                onChange={handleChange}
+              >
+                <option value="" hidden>
+                  Account Type
+                </option>
+                {accountOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
           <div className="form-group">
             {/* <label className="form-label">Profile Picture</label>
             <input
@@ -168,16 +174,17 @@ const Signup = () => {
             /> */}
           </div>
 
-          <div className="button-container">
-            <button type="submit" className="btn-primary">
+          <div className="button-container mb-3">
+            <button type="submit" className="w-100 btn-primary">
               Submit
             </button>
           </div>
-          <div className="link-container">
-            <Link to="/">Login</Link>
+          <div className="text-center link-container">
+            Already have an account? <Link to="/">Login</Link>
           </div>
         </form>
       </div>
+      <div className="col-6 right bg-secondary"></div>
     </div>
   );
 };
