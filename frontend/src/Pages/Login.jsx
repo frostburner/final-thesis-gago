23b5b3e063b6c619dcf8661df4056d5b5e28809e
AeonFlux022 <tolentinokevinjan22@gmail.com>
2024-04-30 00:29:19 +0800

    login ui

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from "../Helpers/AuthContext";
 const Login = () => {
   const navigate = useNavigate();
   const { authState, setAuthState } = useContext(AuthContext);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [alert, setAlert] = useState(null);
   const [formData, setFormData] = useState({
     username: "",
@@ -17,10 +17,17 @@ const Login = () => {
     setAlert(null);
   };
 
- 
-
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Check if username or password is empty
+    if (!formData.username || !formData.password) {
+      setAlert({
+        type: "danger",
+        message: "Please enter both username and password.",
+      });
+      return;
+    }
 
     try {
       const response = await axios.post(
@@ -29,8 +36,8 @@ const Login = () => {
       );
 
       if (response.data.error) {
-        console.log(response.data.error)
-        setAlert({ type: 'danger', message: response.data.error });
+        console.log(response.data.error);
+        setAlert({ type: "danger", message: response.data.error });
       } else {
         localStorage.setItem("accessToken", response.data.token);
         setAuthState({
@@ -47,7 +54,6 @@ const Login = () => {
         } else {
           navigate("/home");
         }
-        
       }
     } catch (error) {
       if (
@@ -55,10 +61,10 @@ const Login = () => {
         error.response.status >= 400 &&
         error.response.status <= 500
       ) {
-        console.log(error.response.data.message)
-        setAlert({ type: 'danger', message: error.response.data.error });
+        console.log(error.response.data.message);
+        setAlert({ type: "danger", message: error.response.data.error });
       } else {
-        setAlert({ type: 'danger', message: "Error creating user." });
+        setAlert({ type: "danger", message: "Error creating user." });
       }
     }
   };
@@ -67,36 +73,63 @@ const Login = () => {
   };
   return (
     <>
- 
       <form onSubmit={handleSubmit} autoComplete="off">
-        <div className="d-flex align-items-center flex-column gap-2 justify-content-center vh-100">
-          <div className="mb-3">
-            <label className="form-label">Username</label>
-            <input
-              type="text"
-              name="username"
-              className="form-control"
-              value={formData.username}
-              onChange={handleChange}
-            />
+        <div className="p-3 d-flex justify-content-start align-items-end vh-100">
+          <div className="p-4 col-5 d-flex flex-column login-container shadow">
+            {alert && (
+              <div className={`alert alert-${alert.type}`} role="alert">
+                {alert.message}
+                <button
+                  type="button" 
+                  className="btn-close"
+                  onClick={closeAlert}
+                />
+              </div>
+            )}
+            <div className="mb-3">
+              <h2 className="fw-bold mb-0">AudioHive</h2>
+              <span>Login to continue</span>
+              <hr></hr>
+            </div>
+            <div className="d-flex gap-2 flex-row mb-3">
+              <div className="flex-grow-1">
+                <label className="form-label">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  className="form-control"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex-grow-1">
+                <label className="form-label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="d-flex flex-column">
+              <div className="">
+                <button className="col-12 mb-2" type="submit">
+                  Login
+                </button>
+                <span>
+                  Don't have an account?{" "}
+                  <Link className="text-decoration-none" path to="/signup">
+                    Sign Up Here
+                  </Link>
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-          <button type="submit">Submit</button>
-          Don't have an account?{" "}
-          <Link path to="/signup">
-            Sign Up Here
-          </Link>
         </div>
       </form>
+      <div className="login-bg"></div>
     </>
   );
 };
