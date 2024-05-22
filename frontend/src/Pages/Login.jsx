6dda23910d73bfd1e9e91e8b343dad10792bbcd5
frostburner <villarafaeljoseph@gmail.com>
2024-05-22 14:46:04 +0800

    commit perf

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,16 +1,138 @@
-import React,{useState,useContext}from "react";import axios from "axios";import{useNavigate,Link}from "react-router-dom";import{AuthContext}from "../Helpers/AuthContext";import "./login.css";const Login=()=>{const navigate=useNavigate();const{authState,setAuthState}=useContext(AuthContext);const[error,setError]=useState("");const[alert,setAlert]=useState(null);const[formData,setFormData]=useState({username:"",password:"",});const closeAlert=()=>{setAlert(null)};const handleSubmit=async(event)=>{event.preventDefault();if(!formData.username||!formData.password){setAlert({type:"danger",message:"Please enter both username and password.",});return}
-try{const response=await axios.post("http://localhost:8080/users/login",formData);if(response.data.error){console.log(response.data.error);setAlert({type:"danger",message:response.data.error})}else{localStorage.setItem("accessToken",response.data.token);setAuthState({id:response.data.id,username:response.data.username,role:response.data.role,status:!0,});console.log("Data stored in localStorage:",response.data);if(response.data.role===0){navigate("/adminDashboard")}else{navigate("/home")}}}catch(error){if(error.response&&error.response.status>=400&&error.response.status<=500){console.log(error.response.data.message);setAlert({type:"danger",message:error.response.data.error})}else{setAlert({type:"danger",message:"Error creating user."})}}};const handleChange=(event)=>{setFormData({...formData,[event.target.name]:event.target.value})};return(<><form onSubmit={handleSubmit}autoComplete="off"><div className="p-3 d-flex justify-content-start align-items-end vh-100"><div className="p-4 col-5 d-flex flex-column login-container shadow">{alert&&(<div className={`alert alert-${alert.type}`}role="alert">{alert.message}<button
-type="button"
-className="btn-close"
-onClick={closeAlert}/></div>)}<div className="mb-3"><h2 className="fw-semi-bold mb-0 logo-text"><img
-src="../../src/assets/logo-small.webp"
-alt=""
-className="logo"/>AudioHive</h2><span>Login to continue</span><hr></hr></div><div className="d-flex gap-2 flex-row mb-3"><div className="form-floating flex-grow-1"><input type="text" name="username" id="floatingInput" placeholder="Username" className="form-control" value={formData.username}
-onChange={handleChange}/><label for="floatingInput">Username</label></div><div className="form-floating flex-grow-1"><input
-type="password"
-name="password"
-id="floatingInput"
-placeholder="Password"
-className="form-control"
-value={formData.password}
-onChange={handleChange}/><label for="floatingInput">Password</label></div></div><div className="d-flex flex-column"><div className=""><button className="col-3 mb-2 button" type="submit">Login</button><span class="mx-3">Don't have an account?{" "}<Link className="text-decoration-none" path to="/signup">Sign Up Here</Link></span></div></div></div></div></form><div className="login-bg"></div></>)};export default Login
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate, Link} from "react-router-dom";
+import { AuthContext } from "../Helpers/AuthContext";
+import "./login.css";
+const Login = () => {
+  const navigate = useNavigate();
+  const { authState, setAuthState } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [alert, setAlert] = useState(null);
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const closeAlert = () => {
+    setAlert(null);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!formData.username || !formData.password) {
+      setAlert({
+        type: "danger",
+        message: "Please enter both username and password.",
+      });
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/users/login",
+        formData
+      );
+      if (response.data.error) {
+        console.log(response.data.error);
+        setAlert({ type: "danger", message: response.data.error });
+      } else {
+        localStorage.setItem("accessToken", response.data.token);
+        setAuthState({
+          id: response.data.id,
+          username: response.data.username,
+          role: response.data.role,
+          status: !0,
+        });
+        console.log("Data stored in localStorage:", response.data);
+        if (response.data.role === 0) {
+          navigate("/adminDashboard");
+        } else {
+          navigate("/home");
+        }
+      }
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        console.log(error.response.data.message);
+        setAlert({ type: "danger", message: error.response.data.error });
+      } else {
+        setAlert({ type: "danger", message: "Error creating user." });
+      }
+    }
+  };
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <div className="p-3 d-flex justify-content-start align-items-end vh-100">
+          <div className="p-4 col-5 d-flex flex-column login-container shadow">
+            {alert && (
+              <div className={`alert alert-${alert.type}`} role="alert">
+                {alert.message}
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={closeAlert}
+                />
+              </div>
+            )}
+            <div className="mb-3">
+              <h2 className="fw-semi-bold mb-0 logo-text">
+                <img
+                  src="../../src/assets/logo-small.webp"
+                  alt="image"
+                  className="logo"
+                />
+                AudioHive
+              </h2>
+              <span>Login to continue</span>
+              <hr></hr>
+            </div>
+            <div className="d-flex gap-2 flex-row mb-3">
+              <div className="form-floating flex-grow-1">
+                <input
+                  type="text"
+                  name="username"
+                  id="floatingInput"
+                  placeholder="Username"
+                  className="form-control"
+                  value={formData.username}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label for="floatingInput">Username</label>
+              </div>
+              <div className="form-floating flex-grow-1">
+                <input
+                  type="password"
+                  name="password"
+                  id="floatingPass"
+                  placeholder="Password"
+                  className="form-control"
+                  value={formData.password}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+                <label for="floatingPass">Password</label>
+              </div>
+            </div>
+            <div className="d-flex flex-column">
+              <div className="">
+                <button className="col-3 mb-2 button" type="submit">
+                  Login
+                </button>
+                <span className="mx-3">
+                  Don't have an account?
+                  <Link className="text-decoration-none" to="/signup">
+                    Sign Up Here    
+                  </Link>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+      <div className="login-bg"></div>
+    </>
+  );
+};
+export default Login;
